b1d453bed7898f2c51562a089de9fed0d9a9b7cd
LiuRenPeng <13263258175@163.com>
2019-02-14 19:46:24 +0800

    add WinMaxArr

---
+++ algorithm/algorithm4j/src/main/java/com/lpcoder/stackAndQueue/WinMaxArr.java
@@ -0,0 +1,55 @@
+package com.lpcoder.stackAndQueue;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import javafx.util.Pair;
+
+/**
+ * @author liurenpeng
+ * @date Created in 19-2-14
+ */
+public class WinMaxArr {
+
+    public static void main(String[] args) {
+        Integer[] arr = {4, 3, 5, 4, 3, 3, 6, 7};
+        int w = 3;
+        Arrays.asList(getWinMaxArr(arr, w)).forEach(System.out::println);
+    }
+
+    public static Integer[] getWinMaxArr(Integer[] arr, int w) {
+        List<Integer> list = Arrays.asList(arr);
+        return getIntervals(arr, w).stream()
+                .map(pair -> list.subList(pair.getKey(), pair.getValue() + 1))
+                .map(subList -> subList.stream().max(Integer::compareTo)
+                        .orElseThrow(() -> new RuntimeException("cannot found max value")))
+                .toArray(Integer[]::new);
+    }
+
+    public static List<Pair<Integer, Integer>> getIntervals(Integer[] arr, int w) {
+        if (w < 1) {
+            throw new RuntimeException("w must gt 1");
+        }
+        if (arr.length == 0) {
+            return Collections.emptyList();
+        }
+        if (arr.length <= w) {
+            return new LinkedList<Pair<Integer, Integer>>() {{
+                add(new Pair<>(1, arr.length - 1));
+            }};
+        }
+        List<Pair<Integer, Integer>> rst = new LinkedList<>();
+        int min = 0;
+        int max = w + min - 1;
+        while (max <= arr.length - 1) {
+            rst.add(new Pair<>(min, max));
+
+            min++;
+            max = w + min - 1;
+        }
+        return rst;
+    }
+
+}
