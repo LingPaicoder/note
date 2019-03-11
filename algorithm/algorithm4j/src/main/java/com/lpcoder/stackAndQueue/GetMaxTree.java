f16bc64ce3a8b9069c8e8480c82c59b6fcbeab8e
LiuRenPeng <13263258175@163.com>
2019-03-11 20:42:07 +0800

    getMaxTree

---
+++ algorithm/algorithm4j/src/main/java/com/lpcoder/stackAndQueue/GetMaxTree.java
@@ -0,0 +1,98 @@
+package com.lpcoder.stackAndQueue;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+import lombok.EqualsAndHashCode;
+
+/**
+ * @author liurenpeng
+ * @date Created in 19-3-11
+ */
+public class GetMaxTree {
+
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 5, 1, 2};
+        Node node = getMaxTree(arr);
+    }
+
+    @EqualsAndHashCode
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static Node getMaxTree(int[] arr) {
+        Node[] nArr = new Node[arr.length];
+        for (int i = 0; i != arr.length; i++) {
+            nArr[i] = new Node(arr[i]);
+        }
+        Stack<Node> stack = new Stack<>();
+        HashMap<Node, Node> lBigMap = new HashMap<>();
+        HashMap<Node, Node> rBigMap = new HashMap<>();
+        for (int i = 0; i != nArr.length; i++) {
+            Node currNode = nArr[i];
+            while ((!stack.isEmpty()) && stack.peek().value < currNode.value) {
+                popStackSetMap(stack, lBigMap);
+            }
+            stack.push(currNode);
+        }
+        while (!stack.isEmpty()) {
+            popStackSetMap(stack, lBigMap);
+        }
+        for (int i = nArr.length - 1; i != -1; i--) {
+            Node currNode = nArr[i];
+            while ((!stack.isEmpty()) && stack.peek().value < currNode.value) {
+                popStackSetMap(stack, rBigMap);
+            }
+            stack.push(currNode);
+        }
+        while (!stack.isEmpty()) {
+            popStackSetMap(stack, rBigMap);
+        }
+        Node head = null;
+        for (int i = 0; i != nArr.length; i++) {
+            Node currNode = nArr[i];
+            Node left = lBigMap.get(currNode);
+            Node right = rBigMap.get(currNode);
+            if (left == null && right == null) {
+                head = currNode;
+            } else if (left == null) {
+                if (right.left == null) {
+                    right.left = currNode;
+                } else {
+                    right.right = currNode;
+                }
+            } else if (right == null) {
+                if (left.left == null) {
+                    left.left = currNode;
+                } else {
+                    left.right = currNode;
+                }
+            } else {
+                Node parent = left.value < right.value ? left : right;
+                if (parent.left == null) {
+                    parent.left = currNode;
+                } else {
+                    parent.right = currNode;
+                }
+            }
+        }
+        return head;
+    }
+
+    public static void popStackSetMap(Stack<Node> stack, HashMap<Node, Node> map) {
+        Node popNode = stack.pop();
+        if (stack.isEmpty()) {
+            map.put(popNode, null);
+        } else {
+            map.put(popNode, stack.peek());
+        }
+    }
+
+}
