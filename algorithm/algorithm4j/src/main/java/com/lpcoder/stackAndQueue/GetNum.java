ff875a5ac5dd3a52220c9131aee1ae254495f19a
LiuRenPeng <13263258175@163.com>
2019-03-18 15:55:59 +0800

    add GetNum

---
+++ algorithm/algorithm4j/src/main/java/com/lpcoder/stackAndQueue/GetNum.java
@@ -0,0 +1,52 @@
+package com.lpcoder.stackAndQueue;
+
+import java.util.LinkedList;
+
+/**
+ * @author liurenpeng
+ * @date Created in 19-3-18
+ */
+public class GetNum {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        System.out.println(getNum(arr, 1));
+    }
+
+    public static int getNum(int[] arr, int num) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+        LinkedList<Integer> qmin = new LinkedList<>();
+        LinkedList<Integer> qmax = new LinkedList<>();
+        int i = 0;
+        int j = 0;
+        int rest = 0;
+        while (i < arr.length) {
+            while (j < arr.length) {
+                while (!qmin.isEmpty() && arr[qmin.peekLast()] >= arr[j]) {
+                    qmin.pollLast();
+                }
+                qmin.addLast(j);
+                while (!qmax.isEmpty() && arr[qmax.peekLast()] <= arr[j]) {
+                    qmax.pollLast();
+                }
+                qmax.addLast(j);
+                if (arr[qmax.getFirst()] - arr[qmin.getFirst()] > num) {
+                    break;
+                }
+                j++;
+            }
+            if (qmin.peekFirst() == i) {
+                qmin.pollFirst();
+            }
+            if (qmax.peekFirst() == i) {
+                qmax.pollFirst();
+            }
+            rest += (j - i);
+            i++;
+        }
+        return rest;
+    }
+
+}
