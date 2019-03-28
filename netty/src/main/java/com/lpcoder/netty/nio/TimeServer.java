b115fc0e7801a36050b5a508a6e6dd34a75957c9
LiuRenPeng <13263258175@163.com>
2019-03-28 20:00:42 +0800

    netty init

---
+++ netty/src/main/java/com/lpcoder/netty/nio/TimeServer.java
@@ -0,0 +1,28 @@
+package com.lpcoder.netty.nio;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+
+/**
+ * @author liurenpeng
+ * @date Created in 19-3-28
+ */
+public class TimeServer {
+
+    private static final int PORT = 8001;
+
+    public static class ReactorTask implements Runnable {
+
+        public void run() {
+
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+    }
+
+}
