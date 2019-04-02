b1f240b20518e9b7072f77f978d9176864c827bb
LiuRenPeng <13263258175@163.com>
2019-04-02 08:59:35 +0800

    fix CLUCache

---
+++ my-kotlin/src/com/lpcoder/my/lru/CLUCache.kt
@@ -0,0 +1,13 @@
+package com.lpcoder.my.lru
+
+import java.util.concurrent.locks.Lock
+import java.util.concurrent.locks.ReentrantLock
+
+/**
+ * @author liurenpeng
+ * @date Created in 19-4-2
+ */
+class CLUCache<K,V>(capacity:Int):LinkedHashMap<K,V>(capacity){
+    private val lock : Lock = ReentrantLock()
+    
+}
