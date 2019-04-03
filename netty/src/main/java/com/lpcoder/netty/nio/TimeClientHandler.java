9a98dfa7cc22be475e7ecd18dad2df64ace2ec4e
LiuRenPeng <13263258175@163.com>
2019-04-03 19:49:54 +0800

    add note

--- netty/src/main/java/com/lpcoder/netty/nio/TimeClientHandler.java
+++ netty/src/main/java/com/lpcoder/netty/nio/TimeClientHandler.java
@@ -25,9 +25,13 @@ public class TimeClientHandler implements Runnable {
         this.host = host == null ? "127.0.0.1" : host;
         this.port = port;
         try {
-            selector = Selector.open();
+            // 1. 打开SocketChannel（绑定客户端本地地址）
             socketChannel = SocketChannel.open();
+            // 2. 设置SocketChannel为非阻塞模式
             socketChannel.configureBlocking(false);
+
+            // 6. 创建Reactor线程，创建多路复用器并启动线程
+            selector = Selector.open();
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
@@ -41,6 +45,7 @@ public class TimeClientHandler implements Runnable {
             e.printStackTrace();
             System.exit(1);
         }
+        // 7. 多路复用器在线程run方法的无限循环体内轮询准备就绪的Key
         while (!stop) {
             try {
                 selector.select(1000);
@@ -79,10 +84,13 @@ public class TimeClientHandler implements Runnable {
 
     private void handleInput(SelectionKey key) throws IOException {
         if (key.isValid()) {
+            // 8. 接收connect事件进行处理
             // 判断是否连接成功
             SocketChannel sc = (SocketChannel) key.channel();
             if (key.isConnectable()) {
+                // 9. 判断链接结果，如果链接成功，注册读事件到多路复用器
                 if (sc.finishConnect()) {
+                    // 10. 注册读事件到多路复用器
                     sc.register(selector, SelectionKey.OP_READ);
                     doWrite(sc);
                 } else {
@@ -91,6 +99,9 @@ public class TimeClientHandler implements Runnable {
                 }
                 if (key.isReadable()) {
                     ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                    // 11. 异步读客户端请求消息到缓冲区
+                    // 12? 对ByteBuffer进行编解码，如果有半包消息接收缓冲区reset，继续读取后续的报文
+                    // 将解码成功的消息封装成Task，投递到业务线程池中，进行业务逻辑编排
                     int readBytes = sc.read(readBuffer);
                     if (readBytes > 0) {
                         readBuffer.flip();
@@ -112,16 +123,21 @@ public class TimeClientHandler implements Runnable {
     }
 
     private void doConnect() throws IOException {
-        // 如果直接连接成功，则注册到多路复用器上，发送请求消息，读应答
-        if (socketChannel.connect(new InetSocketAddress(host, port))) {
+        // 3. 异步连接服务端
+        boolean connected = socketChannel.connect(new InetSocketAddress(host, port));
+        if (connected) {
+            // 4. 判断是否连接成功，如果链接成功，则直接注册读状态位到多路复用器中，发送请求消息，读应答
             socketChannel.register(selector, SelectionKey.OP_READ);
             doWrite(socketChannel);
         } else {
+            // 5. 异步链接返回false，说明客户端已经发送sync包，服务端没有返回ack包，物理链路还没有建立
+            // 如果没有连接成功，向Reactor线程的多路复用器注册OP_CONNECT状态位，监听服务端的TCP ACK应答
             socketChannel.register(selector, SelectionKey.OP_CONNECT);
         }
     }
 
     private void doWrite(SocketChannel sc) throws IOException {
+        // 13. 将POJO对象encode成ByteBuffer，调用SocketChannel的异步write接口，将消息异步发送给客户端
         byte[] req = "QUERY TIME ORDER".getBytes();
         ByteBuffer writeBuffer = ByteBuffer.allocate(req.length);
         writeBuffer.put(req);
