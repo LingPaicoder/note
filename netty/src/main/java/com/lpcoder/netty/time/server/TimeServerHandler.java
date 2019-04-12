425663e26c63e7f700505cd8b724770e1fdaecb5
LiuRenPeng <13263258175@163.com>
2019-04-12 19:58:55 +0800

    netty base end

--- netty/src/main/java/com/lpcoder/netty/time/server/TimeServerHandler.java
+++ netty/src/main/java/com/lpcoder/netty/time/server/TimeServerHandler.java
@@ -1,5 +1,11 @@
 package com.lpcoder.netty.time.server;
 
+import com.lpcoder.Constants;
+
+import java.util.Date;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
 
@@ -11,6 +17,24 @@ public class TimeServerHandler extends ChannelHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        super.channelRead(ctx, msg);
+        ByteBuf buf = (ByteBuf) msg;
+        byte[] req = new byte[buf.readableBytes()];
+        buf.readBytes(req);
+        String body = new String(req, "UTF-8");
+        System.out.println("The time server receive order : " + body);
+        String currentTime = Constants.QUERY_TIME_ORDER_STR.equalsIgnoreCase(body) ?
+                new Date().toString() : "BAD ORDER";
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+        ctx.write(resp);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
     }
 }
