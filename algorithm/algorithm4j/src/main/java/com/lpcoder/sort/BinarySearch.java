b8743eee689abb3cde05e548fdb5190487377e5d
LiuRenPeng <13263258175@163.com>
2019-05-07 23:30:45 +0800

    add binarySearch

---
+++ algorithm/algorithm4j/src/main/java/com/lpcoder/sort/BinarySearch.java
@@ -0,0 +1,33 @@
+package com.lpcoder.sort;
+
+/**
+ * 二分查找/折半查找
+ *
+ * @author liurenpeng
+ * @date Created in 19-5-7
+ */
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{0, 1, 16, 24, 35, 47, 59, 62, 73, 88, 99};
+        System.out.println(binarySearch(arr, 1));
+    }
+
+    private static int binarySearch(int[] arr, int key) {
+        int low = 0;
+        int high = arr.length - 1;
+        int mid;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (arr[mid] == key) {
+                return key;
+            } else if (arr[mid] < key) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+}
