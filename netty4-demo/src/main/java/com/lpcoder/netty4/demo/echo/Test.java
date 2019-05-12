b708b9593eeb1e11db81f85482eb0e2a660ca662
LiuRenPeng <13263258175@163.com>
2019-05-12 14:40:53 +0800

    add nettt4 test

---
+++ netty4-demo/src/main/java/com/lpcoder/netty4/demo/echo/Test.java
@@ -0,0 +1,12 @@
+package com.lpcoder.netty4.demo.echo;
+
+/**
+ * @author liurenpeng
+ * @date Created in 19-5-4
+ */
+public class Test {
+
+    public static void main(String[] args) {
+    }
+
+}
